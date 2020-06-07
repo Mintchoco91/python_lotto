f0d2f3c1a7d9654dabe0d22fd077bfaff564472a
- <boy0221@hanmail.net>
2020-06-07 05:23:19 +0000

    commit

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks.test"""
+"""Django's command-line utility for administrative tasks.testkw"""
 import os
 import sys
 
