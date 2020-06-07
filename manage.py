3c35b584692b67e95066acfdc4d8b939a5f34b74
이준호 <ljuho@naver.com>
2020-06-07 05:34:29 +0000

    test2

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks.testkwffdddddddddddddd"""
+"""Django's command-line utility for administrative tasks.testkwffdddddddddddddd12321124"""
 import os
 import sys
 
